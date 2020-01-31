07d1826ba81c25e2a3881985f4dc6dbe9ac3f11c
Vladimir Emelchenkov <emelchenkov@gmail.com>
2020-01-31 23:09:35 +0300

    setup package with roxygen2
    * implement function add
    * make help to function add

---
+++ R/dcleaner.R
@@ -0,0 +1,11 @@
+#' dcleaner: A package for cleanning and mungling data, importing and exporting data to DB.
+#'
+#' The dcleaner package provides three categories of important functions:
+#' load_table, import_table, clean_table
+#'
+#' @section Loading files:
+#' dcleaner could work with excel file with additional files which describes their schemas.
+#'
+#' @docType  package
+#' @name dcleaner
+NULL
